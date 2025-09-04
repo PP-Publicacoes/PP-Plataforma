75d75b646894adf5ee340450c279d86469e084ae
Ian Patrick <162713721+ianptkcs@users.noreply.github.com>
2025-09-03 23:34:20 -0300

    problema na criação da db resolvido (schema)

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,6 @@
 import { dev } from '$app/environment';
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
@@ -12,4 +11,4 @@ const client = createClient({
   authToken: env.DATABASE_AUTH_TOKEN,
 });
 
-export const db = drizzle(client, { schema });
+export const db = drizzle(client);
